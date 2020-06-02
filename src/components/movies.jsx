b357bbacdccf45f08331d90a2c282b36e935cdda
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-02 11:33:56 +0100

    Add data pagination functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Pagination from './common/pagination';
+import { paginate } from '../utils/paginate';
 import { getMovies } from '../services/fakeMovieService';
+
 
 
 
 class Movies extends Component {
     state = {
         movies: getMovies(),
+        currentPage: 1,
         pageSize: 4
+
     };
 
     handleDelete = (movie) => {
@@ -16,18 +20,19 @@ class Movies extends Component {
     };
 
     handlePageChange = page => {
-        console.log(page);
+        this.setState({ currentPage: page });
     }
 
 
 
     render() {
         const { length: count } = this.state.movies;
-
-
+        const { pageSize, currentPage, movies: allMovies } = this.state;
 
         if (count === 0)
             return <p>There are no movies in the database</p>
+
+        const movies = paginate(allMovies, currentPage, pageSize);
 
         return (
             <React.Fragment>
@@ -43,7 +48,7 @@ class Movies extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.movies.map(movie =>
+                        {movies.map(movie =>
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
@@ -58,7 +63,8 @@ class Movies extends Component {
                 </table>
                 <Pagination
                     itemsCount={count}
-                    pageSize={this.state.pageSize}
+                    pageSize={pageSize}
+                    currentPage={currentPage}
                     onPageChange={this.handlePageChange}
                 />
             </React.Fragment>
