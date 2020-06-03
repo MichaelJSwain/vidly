0f048ecb141d454085dfccaf607c85d794da2bb1
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-03 10:16:06 +0100

    refactoring SFCs to destructure arguments passed to them

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Like = props => {
+const Like = ({ liked, onLike }) => {
     let classes = "fa fa-heart";
-    if (!props.liked) classes += "-o";
+    if (!liked) classes += "-o";
     return (
         <i
-            onClick={props.onLike}
+            onClick={onLike}
             style={{ cursor: "pointer" }}
             className={classes}
             aria-hidden="true"
