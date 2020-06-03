76fcbefd2eefc01c6366fc154b4a4f34c7e24de1
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-03 11:56:37 +0100

    add navbar, pages and setup routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import MovieForm from './components/movieForm';
+import NavBar from './components/navBar';
 import './App.css';
 
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
+
 
 export default App;
