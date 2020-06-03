3f5cb1ee311de69767af85649d1acfc20670052b
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-03 06:41:45 +0100

    extract movies table as a seperate component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Pagination from './common/pagination';
 import ListGroup from './common/listGroup';
+import MoviesTable from './moviesTable';
 import { paginate } from '../utils/paginate';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
+
+
 
 
 
@@ -69,30 +72,10 @@ class Movies extends Component {
                 </div>
                 <div className="col">
                     <p>Showing {filtered.length} movies in the database</p>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.map(movie =>
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-group-sm">Delete</button>
-                                    </td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
+                    <MoviesTable
+                        movies={movies}
+                        onDelete={this.handleDelete}
+                    />
                     <Pagination
                         itemsCount={filtered.length}
                         pageSize={pageSize}
