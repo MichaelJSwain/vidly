0ab012a6d68c2e109bc13b88e4ce14f70d1f6b09
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-04 07:35:26 +0100

    add the login page, routing and form

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import MovieForm from './components/movieForm';
 import NavBar from './components/navBar';
+import LoginForm from './components/loginForm';
 import './App.css';
-
-
 
 class App extends Component {
   render() {
@@ -17,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
